d5089e1ece2a1cdc034c0e376d11bae45f622e5a
ps2pic.top <ps2pic@126.com>
2020-04-04 20:43:19 +0800

    Add Heart

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
 let btns = document.querySelectorAll('button');
 let mask = document.querySelector('.mask');
+let supers = document.querySelectorAll('.super');
+
+supers[0].style.color = 'red';
+supers[1].style.color = 'orangered';
+supers[2].style.color = 'orangered';
+
 
 for (let i = 0; i < btns.length; i++) {
     btns[i].onclick = function () {
