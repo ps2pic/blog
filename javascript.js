a27100d94acbff8a226eb8bb86886ba8bd1f319c
ps2pic.top <ps2pic@126.com>
2020-04-04 14:47:52 +0800

    add contact

--- javascript.js
+++ javascript.js
@@ -1,12 +1,5 @@
 let btns = document.querySelectorAll('button');
 let mask = document.querySelector('.mask');
-let symbol = document.querySelector('.right');
-let menu = document.querySelector('.menu');
-let alinks = document.querySelectorAll('.menu a');
-
-let xL = menu.offsetLeft;
-console.log(xL);
-
 
 for (let i = 0; i < btns.length; i++) {
     btns[i].onclick = function () {
@@ -16,18 +9,4 @@ for (let i = 0; i < btns.length; i++) {
 
 mask.onclick = function () {
     mask.style.display = 'none';
-}
-
-symbol.onclick = function () {
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-}
-
-for (let i = 0; i < alinks.length; i++) {
-    alinks[i].onclick = function () {
-        menu.style.display = 'none';
-    }
 }
